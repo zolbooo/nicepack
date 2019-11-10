bf6b7bce32524334b6b13e8aaaa089b55ab54aef
Zolboo Gantumur <zaccess33rus@gmail.com>
2019-11-11 00:01:42 +0800

    Add @babel/plugin-proposal-export-default-from plugin

--- core/config/babel.config.js
+++ core/config/babel.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         modules: false,
-        useBuiltIns: "entry",
-        targets: "> 0.25%, not dead"
-      }
-    ]
+        useBuiltIns: 'entry',
+        targets: '> 0.25%, not dead',
+      },
+    ],
   ],
   plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-proposal-class-properties"
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
   ],
-  sourceMaps: true
+  sourceMaps: true,
 };
