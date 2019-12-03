e18ef1c3eabe3fef644ff3d9608271f1d65493f8
Zolboo Gantumur <zaccess33rus@gmail.com>
2019-12-03 12:55:18 +0800

    Add typescript plugin

---
+++ typescript/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = config => {
+  config.babel.plugins.push([
+    '@babel/plugin-transform-typescript',
+    { isTSX: true, allExtensions: true },
+  ]);
+  config.extensions = ['.ts', '.tsx'];
+  config.modules[0].test = /\.(tsx?|jsx?)$/;
+  config.entry = () => {
+    const paths = [
+      path.join(process.cwd(), 'src', 'index.ts'),
+      path.join(process.cwd(), 'src', 'index.tsx'),
+      path.join(process.cwd(), 'src', 'index.js'),
+      path.join(process.cwd(), 'src', 'index.jsx'),
+    ];
+    return paths.find(path => fs.existsSync(path));
+  };
+};
