01963c2389811d5e7a78452d5f33fc0bcbe5c7f3
Zolboo Gantumur <zaccess33rus@gmail.com>
2019-12-25 19:28:20 +0800

    Implement React fast refresh

--- react/index.js
+++ react/index.js
@@ -1,3 +1,10 @@
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = config => {
-  config.babel.presets.push("@babel/preset-react");
+  config.babel.presets.push('@babel/preset-react');
+  if (process.env.NODE_ENV === 'development') {
+    config.babel.presets.push('react-refresh/babel');
+    config.plugins.push(new ReactRefreshPlugin({ disableRefreshCheck: true }));
+    config.devServer.hot = true;
+  }
 };
