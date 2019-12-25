647535340937f68cb1e70d4f90c6c2fb5edf0a7a
Zolboo Gantumur <zaccess33rus@gmail.com>
2019-12-25 19:31:13 +0800

    Fix environment check in react plugin

--- react/index.js
+++ react/index.js
@@ -2,7 +2,7 @@ const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = config => {
   config.babel.presets.push('@babel/preset-react');
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV !== 'production') {
     config.babel.presets.push('react-refresh/babel');
     config.plugins.push(new ReactRefreshPlugin({ disableRefreshCheck: true }));
     config.devServer.hot = true;
