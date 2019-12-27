b4c097bbf83514cd4a897159169ae65482fdc229
Zolboo Gantumur <zaccess33rus@gmail.com>
2019-12-27 18:08:45 +0800

    Add ESLint preset

---
+++ eslint-ts/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off',
+    'import/newline-after-import': 'off',
+    '@typescript-eslint/indent': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'global-require': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ eslint-ts/react.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'import/newline-after-import': 'off',
+    '@typescript-eslint/indent': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'global-require': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+  },
+};
